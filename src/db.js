fcf753bf02a51ded07bce65b35118bfdb863c439
Arun <arunprajapati8502@gmail.com>
2023-12-02 18:39:38 +0530

    last and final commit

--- src/db.js
+++ src/db.js
@@ -1,18 +1,21 @@
 import mongoose from "mongoose";
 
-
-const URI = process.env.MONGO_URI ;
+const URI = process.env.MONGO_URI;
 
 const dbconnect = async () => {
-    try {
-        await mongoose.connect(URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("DB connected......");
-    } catch (error) {
-        console.error("Error connecting to the database:", error.message);
-    }
+  let cachedDB = null;
+
+  if (cachedDB) {
+    return cachedDB;
+  }else{
+     const newDB = await mongoose.connect(URI, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+     });
+     cachedDB = newDB;
+     return newDB;
+  }
+ 
 };
 
 export default dbconnect;
