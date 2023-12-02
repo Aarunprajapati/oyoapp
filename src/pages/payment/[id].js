b91f3298a5ad5f8fb6f7375ffed8edee200c148b
Arun <arunprajapati8502@gmail.com>
2023-12-02 12:46:08 +0530

    last commit

--- src/pages/payment/[id].js
+++ src/pages/payment/[id].js
@@ -1,44 +1,54 @@
-import axios from 'axios'
+import axios from 'axios';
 import { useRouter } from 'next/router';
 import Script from 'next/script';
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
-const  payment = () => {
-  const router = useRouter()
-    const makepayment = async()=>{
+const Payment = () => {
+  const router = useRouter();
+
+  const makePayment = async () => {
+    try {
       const val = {
-        id:router.query?.id,
-      }
-    const {data} = await axios.post(`/api/razorpay`, val);
-    console.log(data);
+        id: router.query?.id,
+      };
 
+      const { data } = await axios.post(`/api/razorpay`, val);
+      console.log(data);
 
-    const option = {
+      const options = {
         key: process.env.RAZOREPAY_KEY,
-        name:"ARUN",
-        description:"Payment for hotel",
-        currency:data.currency,
-        amount:data.amount,
+        name: 'ARUN',
+        description: 'Payment for hotel',
+        currency: data.currency,
+        amount: data.amount,
         order_id: data.id,
-        handler:function(response){},
-        prefill:{
-            name:"Arun",
-            email:"lovearun010@gmail.com",
-            contact:8502066002,
+        handler: function (response) {
+          // Handle the response as needed
+          console.log(response);
         },
+        prefill: {
+          name: 'Arun',
+          email: 'lovearun010@gmail.com',
+          contact: 8502066002,
+        },
+      };
+
+      const paymentObj = new window.Razorpay(options);
+      paymentObj.open();
+    } catch (error) {
+      console.error('Error making payment:', error);
     }
-        const paymentObj = new window.Razorpay(option);
-        paymentObj.open();
-}
-    useEffect(()=>{
-        makepayment();
-    },[])
+  };
+
+  useEffect(() => {
+    makePayment();
+  }); // Ensure the useEffect runs only once on component mount
 
   return (
     <>
-      <Script src='http://checkout.razorpay.com/v1/checkout.js'/>
+      <Script src={`https://checkout.razorpay.com/v1/checkout.js`} />
     </>
-  )
-}
+  );
+};
 
-export default payment 
+export default Payment;
